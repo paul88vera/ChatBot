666c9b8c21c6926054a6d1bb106d5b3e79ed033e
paul88vera <paul88vera@gmail.com>
2025-11-16 12:54:19 -0600

    Ai route corrected

--- server/testData.js
+++ server/testData.js
@@ -1,6 +1,6 @@
 // up to 10 FAQS - Description can describe services and prices
 export const company = {
-  ownerId: "org_35XazIKELpblmzUAy6SKLQRCLqa",
+  ownerId: process.env.VITE_TEMP_ORGID,
   companyName: "Verafied Tech",
   companyEmail: "support@verafied.tech",
   companyWebsite: "VERAfied.Tech", // what shows on copyright section
